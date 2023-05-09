fbb69b49a076bbff8c9afa8b944343c341b7cc33
Karla Azucena <karla29.vazquezl@gmail.com>
2023-05-09 09:47:36 -0600

    Actualizar comentarios

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,87 +1,87 @@
 // App name
-const String app_name = 'Attendance with QR Code';
+const String app_name = 'Asistencias con código Qr';
 
 // About page
-const String about_title = 'About';
+const String about_title = 'Acerca de';
 const String about_app_name = '$app_name';
 const String about_developer = 'Developer: MuliaTech';
-const String about_url = 'https://connectwithdev.com';
+const String about_url = 'https://acessoitesi.com';
 const String about_desc =
-    '$app_name is a app focused on fast and easy way keep track of attendance of your users and access your data of your users with website base.';
+    '$app_name Es una aplicación enfocada en una forma rápida y fácil de realizar un seguimiento de la asistencia de sus usuarios y acceder a los datos de sus usuarios con base de sitios web.';
 
 // Attendance page
-const String attendance_not_accurate = 'M (Not accurate).';
-const String attendance_accurate = 'M (Accurate).';
-const String attendance_show_alert = 'Yes, check';
-const String attendance_success_ms = 'success !';
-const String attendance_error_server = 'Sorry, Something Went Wrong !';
-const String attendance_sending = 'Sending...';
-const String attendance_accurate_info = 'Accuracy:';
-const String attendance_on_gps = '\nMake sure turn on your GPS.';
-const String attendance_button_info = 'Click Scan QR to make check';
+const String attendance_not_accurate = 'M (No es correcto).';
+const String attendance_accurate = 'M (Correcto).';
+const String attendance_show_alert = 'Si, cheque';
+const String attendance_success_ms = 'Exito!';
+const String attendance_error_server = 'Lo sentimos, algo salió mal!';
+const String attendance_sending = 'Envio...';
+const String attendance_accurate_info = 'Exactitud:';
+const String attendance_on_gps = '\nAcceder a la ubicación con GPS.';
+const String attendance_button_info = 'Haga clic en Escanear QR para hacer la comprobación';
 const String fake_gps =
-    "We detect you using fake GPS, please turn of your fake GPS.";
-const String check_mock = "Please wait we checking you use fake GPS or not...";
+    "Te detectamos usando GPS falso, por favor desactiva tu GPS falso.";
+const String check_mock = "Por favor, espere a que comprobemos que usa GPS falsa o no...";
 
 // Button
-const String button_scan = 'Scan QR';
+const String button_scan = 'Escanear QR';
 
 // Barcode camera
-const String format_barcode_wrong = 'The format of barcode is wrong.';
+const String format_barcode_wrong = 'El formato del código de barras es incorrecto.';
 const String check_in_first =
-    "You haven't checked-in yet, please check-in first.";
-const String already_check_in = "You have checked-in.";
-const String barcode_empty = 'The barcode is empty.';
+    ".Aún no se ha registrado, primero regístrese";
+const String already_check_in = "Te has registrado.";
+const String barcode_empty = 'El código de barra esta vacio.';
 const String camera_permission =
-    'Sorry, will you give me permission to use the camera?';
+    'Lo sentimos, ¿me darán permiso para usar la cámara?';
 const String barcode_unknown_error = 'Unknown error';
 const String barcode_permission_cam_close =
-    'Sorry, will you give me permission to use the camera, to scan the Qr code? Touch outside the box to close the popup. ';
+    'Lo siento, ¿me darás permiso para usar la cámara, para escanear el código Qr? Toque fuera del cuadro para cerrar la ventana emergente. ';
 
 // Main Menu
-const String main_menu_hi = 'Hi & Welcome!';
+const String main_menu_hi = 'Hola Bienvenido!';
 const String main_menu_title = '$app_name';
-const String main_menu_check_in = 'Check-In';
-const String main_menu_check_in_title = 'Attendance Check-In';
-const String main_menu_check_out = 'Check-Out';
-const String main_menu_check_out_title = 'Attendance Check-Out';
-const String main_menu_settings = 'Settings';
-const String main_menu_report = 'Report';
-const String main_menu_about = 'About';
-const String main_menu_check_in_dec = 'Check-in for your attendance.';
-const String main_menu_check_out_dec = 'Check-out to complete your attendance.';
+const String main_menu_check_in = 'Registrar entrada';
+const String main_menu_check_in_title = 'la entrada se ha registrado';
+const String main_menu_check_out = 'Registrar salida';
+const String main_menu_check_out_title = 'la salida se ha registrado';
+const String main_menu_settings = 'configuraciones';
+const String main_menu_report = 'Reporte';
+const String main_menu_about = 'Acerca de';
+const String main_menu_check_in_dec = 'Checar su entrada  para su asistencia.';
+const String main_menu_check_out_dec = 'Checar salida para completar su asistencia.';
 const String main_menu_settings_dec =
-    'Application settings, URL and KEY with QR code.';
-const String main_menu_report_dec = 'View your attendance report.';
-const String main_menu_about_dec = 'About this App.';
-const String main_menu_version_dec = 'Version of the App.';
-const String main_menu_popup_gps_title = 'Can`t get current location.';
+    'Configuración de la aplicación, URL y KEY con código QR.';
+const String main_menu_report_dec = 'Ver tu informe de asistencia.';
+const String main_menu_about_dec = 'Acerca de la App.';
+const String main_menu_version_dec = 'Version de la  App.';
+const String main_menu_popup_gps_title = '.No se puede obtener la ubicación actual';
 const String main_menu_popup_gps_desc =
-    'Please make sure your enable GPS and try again.';
-const String main_menu_popup_gps_button = 'Okay';
+    'Asegúrese de habilitar el GPS e inténtelo de nuevo.';
+const String main_menu_popup_gps_button = 'Esta bien';
 
 // Report page
-const String report_name = 'NAME';
-const String report_date = 'DATE';
-const String report_time = 'TIME';
-const String report_type = 'TYPE';
-const String report_location = 'LOCATION';
-const String report_no_data = 'No Data Found';
-const String report_title = 'Report';
-const String report_filter_date_from = 'Date From';
-const String report_filter_date_to = 'Date To';
-const String report_filter_by_title = 'Filter By Date';
-const String report_filter_total = 'Total Data: ';
-const String report_filter_input_empty = 'Date is required';
+const String report_name = 'Nombre';
+const String report_date = 'Fecha';
+const String report_time = 'Tiempo';
+const String report_type = 'Tipo';
+const String report_location = 'Ubicación';
+const String report_no_data = 'No se encontraron datos';
+const String report_title = 'Reporte';
+const String report_filter_date_from = 'Fecha de';
+const String report_filter_date_to = 'Fecha a';
+const String report_filter_by_title = 'Filtrar por fecha';
+const String report_filter_total = 'Total de Datos: ';
+const String report_filter_input_empty = 'la fecha es obligatoria';
 
 // Settings
-const String setting_welcome_title = 'Welcome to \n$app_name !';
+const String setting_welcome_title = 'Bienvenido a \n$app_name !';
 const String setting_desc =
-    'First, setting the URL and KEY, please scan the QR code from the management system to get the URL and KEY by clicking "Scan QR".';
+    'Primero, configure la URL y la CLAVE, escanee el código QR del sistema de administración para obtener la URL y la CLAVE haciendo clic en "Escanear QR".';
 
-const String setting_title = 'Settings';
+const String setting_title = 'Configuración';
 const String setting_info =
-    'Please scan the QR code from the management system to get the URL and KEY by clicking "Scan QR".';
+    'Escanee el código QR desde el sistema de gestión para obtener la URL y la CLAVE haciendo clic en "Escanear QR".';
 
-const String ok_text = 'OK';
+const String ok_text = 'Esta bien';
 const String main_title = '$app_name';
